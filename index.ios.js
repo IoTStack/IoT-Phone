55c01147aaa01fcea04571a76ae97c36b9883771
Jordan ( j8 ) <jordan@relayr.de>
2016-03-20 03:48:59 +0100

    fully working geo data

--- index.ios.js
+++ index.ios.js
@@ -16,13 +16,15 @@ import React, {
 } from 'react-native';
 
 import mqtt from 'react-native-mqtt';
-var deviceId = '0b149cd1-ca2f-43c1-a853-a501d51e5748';
 
 var {
     Accelerometer,
     Gyroscope,
     Magnetometer
 } = require('NativeModules');
+
+var deviceId = '0b149cd1-ca2f-43c1-a853-a501d51e5748';
+var mqttClient;
 
 // Set mqtt connection
 
@@ -43,60 +45,23 @@ var relayr = {
 
 mqtt.createClient(relayr).then(function(client) {
 
+  mqttClient = client;
+
   client.on('closed', function(msg) {
     alert('mqtt.event.closed' + msg);
-
   });
 
   client.on('error', function(msg) {
     alert('mqtt.event.error' + msg);
-
-    console.log('mqtt.event.error', msg);
-
   });
 
   client.on('message', function(msg) {
     alert('msg' + msg)
-    console.log('mqtt.event.message', msg);
   });
 
   client.on('connect', function() {
     alert('connected');
     client.subscribe("/v1/" + deviceId+ "/cmd", 0);
-
-        //simple timer to send a message every 1 second
-        var publisher = setInterval(function(){
-
-
-          var states = [ "heat", "cool", "heat-cool", "off" ];
-          var booleanValues = ["true", "false"];
-          var color = {red:(Math.random() * 255), blue: (Math.random() * 255), green: (Math.random() * 255) };
-
-
-          // publish a message to a topic
-          var data = [JSON.stringify({meaning:"color", value: color}),
-                    JSON.stringify({meaning:"temperature", value: Math.floor((Math.random() * 100) + 1)}),
-                    JSON.stringify({meaning:"temperature", path: 'outdoors', value: Math.floor((Math.random() * 100) + 1)}),
-                    JSON.stringify({meaning:"humidity", value: Math.floor((Math.random() * 100) + 1)}),
-                    JSON.stringify({meaning:"proximity", value: Math.floor((Math.random() * 100) + 1)}),
-                    JSON.stringify({meaning:"luminosity", value: Math.floor((Math.random() * 100) + 1)}),
-                    JSON.stringify({meaning:"noiseLevel", value: Math.floor((Math.random() * 100) + 1)}),
-                    JSON.stringify({meaning:"random number", value: Math.floor((Math.random() * 60) + 1)}),
-                    JSON.stringify({meaning:"random string", value: "Random long long long string with a number aanost euhsnao heuns aoeuhtasnoteh unsahoenustah oenush aontseh unsaoeh uhaoesuh " + Math.floor((Math.random() * 60) + 1)}),
-                    JSON.stringify({meaning:"random percentage", value: Math.floor((Math.random() * 100) + 1)}),
-                    JSON.stringify({meaning:"hvac mode", value: states[Math.floor((Math.random() * 3) + 0)]}),
-                    JSON.stringify({meaning:"fan_timer_active", path:"fan_timer_active", value: Math.floor((Math.random() * 2) + 0) == 1}),
-                    JSON.stringify({meaning:"rssi", value: (Math.random() * 100)}),
-                    JSON.stringify({meaning:"angularSpeed", value: {x: Math.floor((Math.random() * 110)), y: Math.floor((Math.random() * 110)), z: Math.floor((Math.random() * 110)) }}),
-                    JSON.stringify({meaning:"acceleration", value: {x: Math.floor((Math.random() * 11)), y: Math.floor((Math.random() * 11)), z: Math.floor((Math.random() * 11)) }}),
-                    JSON.stringify({"meaning":"error","value":{"errorMessage":'The message is not in json format : \"[{\"meaning\": \"temperature\", \"value\": NaN}, {\"meaning\": \"humidity\", \"value\": 0.0}, {\"meaning\": \"luminance\", \"value\": 242}, {\"meaning\": \"noiseLevel\", \"value\": 2}]\"'}})
-                ];
-
-          data.forEach(function(dataPoint) {
-            client.publish("/v1/" + deviceId + "/data", dataPoint, 0, false);
-          });
-      }, 1000);
-
   });
 
   client.connect()
@@ -106,14 +71,14 @@ mqtt.createClient(relayr).then(function(client) {
 });
 
 // Set timing intervals
-// Accelerometer.setAccelerometerUpdateInterval(0.1); // in seconds
-// Gyroscope.setGyroUpdateInterval(0.1);
-// Magnetometer.setMagnetometerUpdateInterval(0.1);
+Accelerometer.setAccelerometerUpdateInterval(0.3); // in seconds
+Gyroscope.setGyroUpdateInterval(0.3);
+Magnetometer.setMagnetometerUpdateInterval(0.3);
 
-// // Set updates
-// Accelerometer.startAccelerometerUpdates();
-// Gyroscope.startGyroUpdates();
-// Magnetometer.startMagnetometerUpdates();
+// Set updates
+Accelerometer.startAccelerometerUpdates();
+Gyroscope.startGyroUpdates();
+Magnetometer.startMagnetometerUpdates();
 
 var AccelerometerManager = React.createClass({
   getInitialState: function () {
@@ -126,6 +91,12 @@ var AccelerometerManager = React.createClass({
   },
   componentDidMount: function () {
     DeviceEventEmitter.addListener('AccelerationData', function (data) {
+
+      if(this.state.gyro) {
+        var mqttData = JSON.stringify({meaning:"accelerometer", value: data})
+        mqttClient.publish("/v1/" + deviceId + "/data", mqttData, 0, false);
+      }
+
       this.setState({
         x: data.acceleration.x.toFixed(5),
         y: data.acceleration.y.toFixed(5),
@@ -178,6 +149,11 @@ var GyroscopeManager = React.createClass({
   },
   componentDidMount: function () {
     DeviceEventEmitter.addListener('GyroData', function (data) {
+      if(this.state.gyro) {
+        var mqttData = JSON.stringify({meaning:"gyroscope", value: data})
+        mqttClient.publish("/v1/" + deviceId + "/data", mqttData, 0, false);
+      }
+
       this.setState({
         x: data.rotationRate.x.toFixed(5),
         y: data.rotationRate.y.toFixed(5),
@@ -231,6 +207,11 @@ var MagnetometerManager = React.createClass({
   },
   componentDidMount: function () {
     DeviceEventEmitter.addListener('MagnetometerData', function (data) {
+      if(this.state.gyro) {
+        var mqttData = JSON.stringify({meaning:"magnetometer", value: data})
+        mqttClient.publish("/v1/" + deviceId + "/data", mqttData, 0, false);
+      }
+
       this.setState({
         x: data.magneticField.x.toFixed(5),
         y: data.magneticField.y.toFixed(5),
@@ -279,9 +260,20 @@ var Geolocation = React.createClass({
     return {
       initialPosition: 'unknown',
       lastPosition: 'unknown',
+      map: true
     };
   },
-
+  handleStart: function () {
+    this.setState({
+      map: true
+    });
+  },
+  handleStop: function () {
+    navigator.geolocation.clearWatch(this.watchID);
+    this.setState({
+      map: false
+    });
+  },
   componentDidMount: function() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
@@ -292,8 +284,14 @@ var Geolocation = React.createClass({
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
     );
     this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lastPosition = JSON.stringify(position.coords);
-      this.setState({lastPosition});
+      var data = JSON.stringify(position.coords);
+      
+      if(this.state.map) {
+        var mqttData = JSON.stringify({meaning:"geo location", value: data})
+        mqttClient.publish("/v1/" + deviceId + "/data", mqttData, 0, false);
+      }
+
+      this.setState({data});
     });
   },
 
@@ -303,14 +301,17 @@ var Geolocation = React.createClass({
 
   render: function() {
     return (
-      <View>
-      <Text style={styles.welcome}>
-        Geolocation
-      </Text>
-        <Text>
-          <Text style={styles.title}>Current position: </Text>
-          {this.state.lastPosition}
+      <View style={styles.widget}>
+        <Text style={styles.welcome}>
+          Geolocation
         </Text>
+        <Text style={styles.title}>Current position: </Text>
+        <Text>{this.state.data}</Text>
+        {
+          <Switch 
+          onValueChange={(value) => (value) ? this.handleStart(): this.handleStop()}
+          value={this.state.map} />
+        }
       </View>
     );
   }
@@ -319,13 +320,17 @@ var Geolocation = React.createClass({
 class IoTPhone extends Component {
   render() {
     return (
-      // <View style={styles.container}>
+      <View style={styles.container}>
         <ScrollView>
         <Text style={styles.welcome}>
           IoT Phone
         </Text>
+          <AccelerometerManager></AccelerometerManager>
+          <GyroscopeManager></GyroscopeManager>
+          <MagnetometerManager></MagnetometerManager>
+          <Geolocation></Geolocation>
         </ScrollView>
-      // </View>
+      </View>
     );
   }
 }
